2c755725bb5947c02312d36f5aed3660b69d57f5
dezimoewi <dezimoewi@gmail.com>
2026-03-25 11:42:55 +0100

    chore: update dependencies and main config

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,23 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { validateEnv, env } from './config/env';
+import { validateEnv } from './config/env';
 import * as express from 'express';
 
 async function bootstrap() {
-
-  validateEnv(); ,
-
+  validateEnv();
   const app = await NestFactory.create(AppModule);
-  
 
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-''
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS?.split(',') || '*',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'apikey', 'Accept', 'Authorization'],
-    credentials: true, 
+    credentials: true,
   });
-  
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
